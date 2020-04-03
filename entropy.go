c8f23e275f956ce6ee430e5fb03e30b99843f95e
sofdg3 <sofdg3@r0b0tnull.sofdg3>
2020-04-02 23:35:21 -0300

    simple info

--- entropy.go
+++ entropy.go
@@ -6,41 +6,22 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 )
 
 func main() {
 	var size int
 	var help string
 
-	flag.IntVar(&size, "size", 16, "")
-	flag.StringVar(&help, "help", "off", "")
-
-	for _, arg := range os.Args {
-		if arg == "help" {
-			menuhelp()
-			os.Exit(0)
-		}
-	}
-
-	if len(os.Args) < 2 {
-		menuhelp()
-		os.Exit(0)
-	}
+	flag.IntVar(&size, "size", 16, "entropy -size 42 \nCreate a seedword with 42 bytes")
+	flag.StringVar(&help, "help", "", "Display this help\n")
 
 	flag.Parse()
 	fmt.Println(seedGen(size))
 }
 
-func menuhelp() {
-	fmt.Println("Usage: \n entropy [options]:")
-	fmt.Println()
-	fmt.Println("	-size 42	Create a seedword with 42 bytes. Default is 16.")
-	fmt.Println()
-}
-
 func seedGen(size int) (result string) {
 	crypto := make([]byte, size)
+
 	_, err := rand.Read(crypto)
 	if err != nil {
 		log.Fatal("Error: ", err)
